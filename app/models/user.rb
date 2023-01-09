10b841002fdfff5bfdc0be7a16bd2111e9d44742
Herri Purba <119906399+HerriUkirama@users.noreply.github.com>
2023-01-09 15:01:47 +0700

    Create Form Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: {in: 4..12}
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: {in: 6..16}, presence: true
 end
